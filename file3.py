bf726de74187df3c00f9e2fe306ef9c479dab9e7
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-01 19:22:04 +0530

    Delete file3.py

--- file3.py
+++
@@ -1,8 +0,0 @@
-print("enter marks of 4 sub")
-math=int(input("math="))
-english=int(input("english="))
-scince=int(input("scince="))
-hindi=int(input("hind="))
-sum=math+english+scince+hindi
-p=(sum/400)*100
-print("the prcentage is=",p)
